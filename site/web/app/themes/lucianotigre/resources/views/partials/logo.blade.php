f33fc3a8882cec6558fcf790d4fdfb14c093a36d
Daniel Semblano <dsemblano@gmail.com>
2025-12-30 15:11:49 -0300

    margin-left for H1 header

--- site/web/app/themes/lucianotigre/resources/views/partials/logo.blade.php
+++ site/web/app/themes/lucianotigre/resources/views/partials/logo.blade.php
@@ -1,4 +1,4 @@
-<a class="flex flex-row max-w-[302px] w-full" href="{{ home_url('/') }}">
+<a class="flex flex-row max-w-[302px] w-full items-center" href="{{ home_url('/') }}">
   <img width="30" height="32" class="mr-2 block hover:scale-105 transition duration-300 ease-in-out" id="logo" alt="Logo machete Luciano Tigre site" src="{{ Vite::asset('resources/images/macheteicon.webp') }}" />
-  <h1 class="font-heading text-primary text-lg font-bold leading-tight tracking-[-0.015em]">Luciano Tigre</h1>
+  <h1 class="font-heading text-primary text-lg font-bold leading-tight tracking-[-0.015em] ml-1.5">Luciano Tigre</h1>
 </a>
